c95e2ce4ad5da723aa51029e8c0d39e9dd415af8
trepear <tbuhrsmith@gmail.com>
2020-07-21 18:13:34 -0500

    jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql');
 const util = require('util');
+var connection;
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection((process.env.JAWSDB_URL));
+} else {
+  connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'Ethangirard96',
     database: 'note_taker_db'
   });
+}
 
 connection.connect(function(err) {
   if (err) {
